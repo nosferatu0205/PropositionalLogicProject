12c5ef016f0c8edbd3acdcdcfe920c74d59274a2
MonayemSarker <bsse1228@iit.du.ac.bd>
2023-09-23 19:14:17 +0600

    working with Ai

--- ai.py
+++ ai.py
@@ -6,7 +6,8 @@ wumpus_probability = numpy.zeros((10,10), dtype=int)
 breeze_location = numpy.zeros((10,10), dtype=bool)
 stench_location = numpy.zeros((10,10), dtype=bool)
 
-def find_next_move(game, agent_position, visited_locations):
+
+def find__move(game, agent_position, visited_locations):
     percepts = []
     percepts = game.get_percepts()
     row,col  = agent_position
@@ -36,3 +37,21 @@ def find_next_move(game, agent_position, visited_locations):
 
 def next_move():
     pass
+
+
+def does_pit_exist(location):
+    if pit_probability[location[0],location[1]] > 2:
+        return True
+    else:
+        return False
+    
+def does_wumpus_exist(location):
+    if wumpus_probability[location[0],location[1]] > 2:
+        return True
+    else:
+        return False
+
+def kill_wumpus():
+    pass
+    # wumpus_probability re 0 kore dite hbe, stench er matrix update hobe
+    
